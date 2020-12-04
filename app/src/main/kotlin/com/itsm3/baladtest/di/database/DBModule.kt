d2fb3607a5249bd867877e4bc66118481b8a8793
m3 <m@gmail.com>
2020-12-04 22:38:13 +0330

    Provide DbModule di

--- app/src/main/kotlin/com/itsm3/baladtest/di/database/DBModule.kt
+++ app/src/main/kotlin/com/itsm3/baladtest/di/database/DBModule.kt
@@ -1,6 +1,19 @@
 package com.itsm3.baladtest.di.database
 
+import androidx.room.Room
+import com.itsm3.baladtest.data.db.explore.ExploreDB
+import com.itsm3.baladtest.di.application.MyApplication
 import dagger.Module
+import dagger.Provides
 
 @Module
-object DbModule {}
+object DbModule {
+    @Provides
+    @JvmStatic
+    fun provideExploreDb(context: MyApplication) =
+        Room.databaseBuilder(context, ExploreDB::class.java, "explore_db").build()
+
+    @Provides
+    @JvmStatic
+    fun provideExploreDao(exploreDB: ExploreDB) = exploreDB.exploreDao()
+}
